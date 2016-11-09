623803b31d2459dd0e9efe575e85ebef55415894
Stephen Lind <stephen.l@getjobber.com>
2016-11-09 16:07:52 -0700

    Layout cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Campaign App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end

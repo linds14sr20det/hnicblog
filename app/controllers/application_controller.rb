4555320eb6227a2d6b5cb54d08cc1b7082b2199a
Stephen Lind <stephen.l@getjobber.com>
2016-11-16 16:15:49 -0700

    Login/logout done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def home
-  end
+  include SessionsHelper
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,11 +29,11 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        log_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render 'new'
       end
     end
   end

bdbfdaa04fed8c6a0c5101ff5bdb5adc4a08fde6
Stephen Lind <stephen.l@getjobber.com>
2016-11-16 16:45:22 -0700

    persistent sessions now available

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114233715) do
+ActiveRecord::Schema.define(version: 20161116232447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20161114233715) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 

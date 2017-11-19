0bcff54278636748369527e983c747ec917b2b02
Stephen Lind <stephen.l@getjobber.com>
2017-11-18 22:08:21 -0700

    add categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119043451) do
+ActiveRecord::Schema.define(version: 20171119050603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "cohorts", force: :cascade do |t|
     t.datetime "start_at"

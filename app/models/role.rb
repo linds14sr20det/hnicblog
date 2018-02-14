8926e0b95a1ba65efef06c50bb04b4d00900a903
Stephen Lind <stephen.l@getjobber.com>
2018-02-14 12:12:23 -0700

    setup ctas for roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,14 @@ class Role < ApplicationRecord
   belongs_to :user
 
   enum role: [:applicant, :judge, :admin]
+
+  def is_admin?
+    return true if role == :admin.to_s
+    false
+    end
+
+  def is_judge?
+    return true if role == :judge.to_s
+    false
+  end
 end

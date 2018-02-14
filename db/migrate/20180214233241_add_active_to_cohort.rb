e61ef38749a18468c6211ec37bc52931e6837e59
Stephen Lind <stephen.l@getjobber.com>
2018-02-14 16:35:23 -0700

    fix silly migration table name error

--- db/migrate/20180214233241_add_active_to_cohort.rb
+++ db/migrate/20180214233241_add_active_to_cohort.rb
@@ -1,5 +1,5 @@
 class AddActiveToCohort < ActiveRecord::Migration[5.0]
   def change
-    add_column :cohort, :active, :boolean, default: false
+    add_column :cohorts, :active, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124195505) do
+ActiveRecord::Schema.define(version: 20180214233241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20171124195505) do
   create_table "cohorts", force: :cascade do |t|
     t.datetime "start_at"
     t.datetime "end_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "active",     default: false
   end
 
   create_table "roles", force: :cascade do |t|

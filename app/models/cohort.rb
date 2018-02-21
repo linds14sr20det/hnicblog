a9dcac03d867ccdc4f4ace9f974eb3ea7be68e47
Stephen Lind <stephen.l@getjobber.com>
2018-02-20 15:17:29 -1000

    re-enable deletion of foreign keys

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,6 +1,6 @@
 class Cohort < ApplicationRecord
   has_many :submissions
-  has_many :categories, inverse_of: :cohort
+  has_many :categories, inverse_of: :cohort, dependent: :destroy
   #TODO: validate start is before end
   accepts_nested_attributes_for :categories, reject_if: :all_blank, allow_destroy: true
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,8 +1,8 @@
 class Submission < ApplicationRecord
   belongs_to :cohort
   belongs_to :user
-  has_many :team_members, inverse_of: :submission
-  has_many :attachments, inverse_of: :submission
-  accepts_nested_attributes_for :attachments, :allow_destroy => true
+  has_many :team_members, inverse_of: :submission, dependent: :destroy
+  has_many :attachments, inverse_of: :submission, dependent: :destroy
+  accepts_nested_attributes_for :attachments, allow_destroy: true
   accepts_nested_attributes_for :team_members, reject_if: :all_blank, allow_destroy: true
 end

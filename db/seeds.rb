cdc76c17a37a5536745bf3ad7ad21fe989aea2eb
Stephen Lind <stephen.l@getjobber.com>
2018-03-09 16:04:52 -0700

    updated container styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,19 @@ User.create!(
     activated: true,
     activated_at: Time.zone.now
 )
+
+User.create!(
+    email:  "Phesmic@gmail.com",
+    name: "Tom Carter",
+    password_digest: "$2a$10$OTvrczPu1h5zpo8.b61peOm6hHNyvu9aji3R1rKCVoR/JhCpPwXee", #password
+    activated: true,
+    activated_at: Time.zone.now
+)
+
+User.create!(
+    email:  "Danbyer@gmail.com",
+    name: "Dan Byer",
+    password_digest: "$2a$10$OTvrczPu1h5zpo8.b61peOm6hHNyvu9aji3R1rKCVoR/JhCpPwXee", #password
+    activated: true,
+    activated_at: Time.zone.now
+)

85495d33a177bd524773c84c349593b958a33559
Stephen Lind <stephen.l@getjobber.com>
2018-03-13 00:24:32 -0600

    fixed initialization to run on new action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :calculate_s3, only: [:edit]
+  before_action :calculate_s3, only: [:edit, :new]
 
   include SessionsHelper
 
